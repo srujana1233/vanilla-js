0d7829da6ca5b628cc7cf441b884f83d21e1122b
srujana1233 <kakunurisrujana@gmail.com>
2021-04-18 16:57:54 +0530

    updated in js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 'use strict';
 
 const btnClick = document.querySelector('.btn-click');
-var a = 0;
-btnClick.addEventListener('click', () => {
-    a = a + 1;
-    console.log(a);
-});
+const userInput = document.querySelector('#user-input');
+const firstNumber = document.querySelector('#first-number');
+const secondNumber = document.querySelector('#second-number');
+const resultNumber = document.querySelector('#result-number');
+const paragraph = document.querySelector('p');
+const textArea = document.querySelector('.textarea');
+
+
+btnClick.addEventListener('click', myFunction);
+
+function myFunction() {
+    var num1 = Number(firstNumber.value);
+    var num2 = Number(secondNumber.value);
+    var result = 0;
+    result = num1 + num2;
+
+    resultNumber.value = result;
+    console.log(result);
+
+}
+textArea.innerHTML = '<h2> hello everyone</h2>';
